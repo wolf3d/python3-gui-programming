b4f1a5ff7f403921876a6de820dd9443c569cb86
wolf3d <oldcar@inbox.lv>
2020-05-17 16:18:29 +0300

    added ValidatedMixin class

--- chapter_04/data_entry_app.py
+++ chapter_04/data_entry_app.py
@@ -189,7 +189,60 @@ class DataRecordForm(tk.Frame):
     def reset(self):
         for widget in self.inputs.values():
             widget.set('')
-            
+
+
+class ValidatedMixin:
+    """adds a validation functionality to an input widget"""
+
+    def __init__(self, *args, error_var=None, **kwargs):
+        self.error = error_var or tk.StringVar()
+        super().__init__(*args, **kwargs)
+
+        vcmd = self.register(self._validate)
+        invcmd = self.register(self._invalid)
+        self.config(validate='all',
+            validatecommand=(vcmd, '%P', '%s', '%S', '%V', '%i', '%d'),
+            invalidcommand=(invcmd, '%P', '%s', '%S', '%V', '%i', '%d')
+        )
+    def _validate(self, proposed, current, char, event, index,
+    action):
+        self._toggle_error(False)
+        self.error.set('')
+        valid = True
+        if event == 'focusout':
+            valid = self._focusout_validate(event=event)
+        elif event == 'key':
+            valid = self._key_validate(proposed=proposed,
+            current=current, char=char, event=event,
+            index=index, action=action)
+        return valid
+    
+    def _focusout_validate(self, **kwargs):
+        return True
+
+    def _key_validate(self, **kwargs):
+        return True
+
+    def _invalid(self, proposed, current, char, event, index,
+        action):
+        if event == 'focusout':
+            self._focusout_invalid(event=event)
+        elif event == 'key':
+            self._key_invalid(proposed=proposed,
+                current=current, char=char, event=event,
+                index=index, action=action)
+
+    def _focusout_invalid(self, **kwargs):
+        self._toggle_error(True)
+
+    def _key_invalid(self, **kwargs):
+        pass
+
+    def trigger_focusout_validation(self):
+        valid = self._validate('', '', '', 'focusout', '', '')
+        if not valid:
+            self._focusout_invalid(event='focusout')
+        return valid
 
 class Application(tk.Tk):
     """Application root window"""
